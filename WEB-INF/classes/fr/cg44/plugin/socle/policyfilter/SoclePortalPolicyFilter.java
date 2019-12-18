1f3f75f8c63934ca230adbfc4ae5af49361e6352
LChoquet <lucas.choquet@trsb.net>
2019-12-18 10:03:49 +0100

    [Feat.] Sous les collapsers, faire un lien direct si la catégorie a un contenu principal

--- WEB-INF/classes/fr/cg44/plugin/socle/policyfilter/SoclePortalPolicyFilter.java
+++ WEB-INF/classes/fr/cg44/plugin/socle/policyfilter/SoclePortalPolicyFilter.java
@@ -20,14 +20,13 @@ import com.jalios.jcms.portlet.PortalManager;
 import com.jalios.util.ServletUtil;
 import com.jalios.util.Util;
 
+import fr.cg44.plugin.socle.SocleUtils;
+
 
 public class SoclePortalPolicyFilter extends BasicPortalPolicyFilter {
 
 	private static final Logger logger = Logger.getLogger(SoclePortalPolicyFilter.class);
-
-	// La catégorie technique qui désigne qu'un contenu est mis en avant si celui-ci y est catégorisé.
-	private static final String MISE_EN_AVANT_CAT_PROP = "$id.plugin.socle.page-principale.cat";
-
+	
 	@Override
 	public boolean init(Plugin plugin){
 		return true;
@@ -43,24 +42,17 @@ public class SoclePortalPolicyFilter extends BasicPortalPolicyFilter {
 
 		// Redirection pour les catégorie avec un contenu "mis en avant" (Contenu appartenant à la catégorie "mis en avant")
 		Category currentCat = getChannel().getCategory(dcp.id);
-		if(currentCat != null && getChannel().getCategory(MISE_EN_AVANT_CAT_PROP) != null) {
-			Member loggedMember = dcp.loggedMember;		
-			QueryHandler qh = new QueryHandler();
-			qh.setCids(getChannel().getProperty(MISE_EN_AVANT_CAT_PROP), currentCat.getId());
-			qh.setExactCat(true);
-			qh.setLoggedMember(loggedMember);
-			qh.setExactCat(true);
-			QueryResultSet result = qh.getResultSet();
-			Publication redirectPub = Util.getFirst(result);		
-			if(Util.notEmpty(redirectPub)) {
-				logger.debug("Publication affichée pour la catégorie " + currentCat.getId() + " : " + redirectPub.getId());								
-				HttpServletRequest request = getChannel().getCurrentServletRequest();
-				HttpServletResponse response = getChannel().getCurrentServletResponse();				
-				Locale userLocale = getChannel().getCurrentUserLocale();				
-				response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
-				response.setHeader("Location", ServletUtil.toAbsoluteUrl(request, redirectPub.getDisplayUrl(userLocale)));				
-			}
-		}	
+		Publication redirectPub = SocleUtils.getContenuPrincipal(currentCat);
+	
+		if(Util.notEmpty(redirectPub)) {
+			logger.debug("Publication affichée pour la catégorie " + currentCat.getId() + " : " + redirectPub.getId());								
+			HttpServletRequest request = getChannel().getCurrentServletRequest();
+			HttpServletResponse response = getChannel().getCurrentServletResponse();				
+			Locale userLocale = getChannel().getCurrentUserLocale();				
+			response.setStatus(HttpServletResponse.SC_MOVED_PERMANENTLY);
+			response.setHeader("Location", ServletUtil.toAbsoluteUrl(request, redirectPub.getDisplayUrl(userLocale)));				
+		}
+		
 	}
 
 	
