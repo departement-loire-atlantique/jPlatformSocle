2825a25181342fc7c020a61b525bdbd50f60c7e3
LChoquet <lucas.choquet@trsb.net>
2020-01-28 16:48:04 +0100

    Ajout de la requête prototype d'extraction de flux

--- WEB-INF/classes/fr/cg44/plugin/socle/infolocale/RequestManager.java
+++ WEB-INF/classes/fr/cg44/plugin/socle/infolocale/RequestManager.java
@@ -2,7 +2,6 @@ package fr.cg44.plugin.socle.infolocale;
 
 import java.io.IOException;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,6 +21,9 @@ public class RequestManager {
 	
 	private static final Logger LOGGER = Logger.getLogger(RequestManager.class);
 	
+	/**
+	 * Envoie une requête pour s'authentifier à Infolocale et générer les tokens d'authentification
+	 */
 	public static void initTokens() {
 		
 		TokenManager tokenManager = TokenManager.getInstance();
@@ -81,6 +83,9 @@ public class RequestManager {
 		}
 	}
 	
+	/**
+	 * Regénère les tokens d'authentification. En cas de token invalide, procède à l'authentification
+	 */
 	public static void regenerateTokens() {
 
 		boolean invalidToken = false;
@@ -143,6 +148,72 @@ public class RequestManager {
 		}
 	}
 	
+	public static JSONObject extractFluxData(String fluxId) {
+		
+		JSONObject fluxData = new JSONObject();
+		
+		TokenManager tokenManager = TokenManager.getInstance();
+		boolean expiredToken = false;
+		
+		Map<String, String> params = new HashMap<String,String>();
+		params.put("Authorization", "Bearer " + tokenManager.getAccessToken());
+		
+		HttpURLConnection connection = null;
+		
+		try {
+			fluxData.put("success", false); // sera remplacé par "true" dans les cas de requête réussie
+			
+			connection = createPostConnection("https://api.infolocale.fr/flux/" + fluxId + "/data", params, "application/x-www-form-urlencoded");
+			
+			if (Util.isEmpty(connection)) {
+				LOGGER.warn("Method extractFluxData => l'élément de connexion est vide, veuillez vérifier la configuration.");
+				return fluxData;
+			}
+			
+			connection.connect();
+			
+			int status = connection.getResponseCode();
+						
+			switch (status) {
+				case 200:
+					String jsonReply = SocleUtils.convertStreamToString(connection.getInputStream());
+					
+					fluxData = new JSONObject(jsonReply);
+					fluxData.put("success", true);
+					
+					LOGGER.debug("Tokens regénérés");
+					break;
+				case 401:
+					LOGGER.warn("Method extractFluxData => code HTTP innatendu " + status + ". Token expiré.");
+					expiredToken = true;
+					fluxData.put("failure_reason", "invalid_token");
+					break;
+				case 404:
+					LOGGER.warn("Method extractFluxData => code HTTP innatendu " + status + ". Flux " + fluxId + " non trouvé.");
+					fluxData.put("failure_reason", "wrong_flux");
+					break;
+				default:
+					LOGGER.warn("Method extractFluxData => code HTTP innatendu " + status + ". Veuillez verifier la configuration.");
+					fluxData.put("failure_reason", "unknown_error");
+					break;
+			}
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
+		} finally {
+			if (Util.notEmpty(connection)) connection.disconnect();
+		}
+		
+		if (expiredToken) {
+			LOGGER.debug("Token invalide. Tentative de regénération de token...");
+			regenerateTokens();
+		}
+		
+		return fluxData;
+	}
+	
 	private static HttpURLConnection createPostConnection(String url, Map<String, String> params, String contentType) {
 		HttpURLConnection connection = null;
 		try {
@@ -156,6 +227,24 @@ public class RequestManager {
 			connection.setRequestProperty("Content-Length", String.valueOf(dataPost.length));
 			
 			connection.getOutputStream().write(dataPost);
+			
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		
+		return connection;
+	}
+	
+	private static HttpURLConnection createGetConnection(String url, Map<String, String> params) {
+		HttpURLConnection connection = null;
+		try {
+			connection = IOUtil.openConnection(new URL(url), true, true, "POST");
+			
+			if (Util.isEmpty(params)) return connection;
+			
+			for (String itKey : params.keySet()) {
+				connection.setRequestProperty(itKey, params.get(itKey));
+			}
 			
 		} catch (IOException e) {
 			e.printStackTrace();
