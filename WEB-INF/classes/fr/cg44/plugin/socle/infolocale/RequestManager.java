10e0003f5ffec0cea6a3a2949daab32b762fa515
LChoquet <lucas.choquet@trsb.net>
2020-03-10 13:25:34 +0100

    Refacto des méthodes de requête

--- WEB-INF/classes/fr/cg44/plugin/socle/infolocale/RequestManager.java
+++ WEB-INF/classes/fr/cg44/plugin/socle/infolocale/RequestManager.java
@@ -38,7 +38,6 @@ public class RequestManager {
     
     private static String methodInitTokensError = "Method initTokens => code HTTP innatendu ";
     private static String methodRegenerateTokensError = "Method regenerateTokens => code HTTP innatendu ";
-    private static String methodExtractFluxDataError = "Method extractFluxData => code HTTP innatendu ";
     private static String methodFilterFluxDataError = "Method filterFluxData => code HTTP innatendu ";
     
     private static String success = "success";
@@ -50,23 +49,40 @@ public class RequestManager {
      * Envoie une requête pour s'authentifier à Infolocale et générer les tokens d'authentification
      */
     public static void initTokens() {
+        sendTokenRequest("https://api.infolocale.fr/auth/signin", "initTokens");
+    }
+    
+    /**
+     * Regénère les tokens d'authentification. En cas de token invalide, procède à l'authentification
+     */
+    public static void regenerateTokens() {
+        sendTokenRequest("https://api.infolocale.fr/auth/refresh-token", "regenerateTokens");
+    }
+    
+    private static void sendTokenRequest(String url, String type) {
+        boolean invalidToken = false;
         
         TokenManager tokenManager = TokenManager.getInstance();
-        Channel channel = Channel.getChannel();
-        
-        String login = channel.getProperty("jcmsplugin.socle.infolocale.login");
-        String password = channel.getProperty("jcmsplugin.socle.infolocale.password");
         
         Map<String, Object> params = new HashMap<>();
-        params.put("login", login);
-        params.put("password", password);
+        Channel channel = Channel.getChannel();
         
         try {
             
-            CloseableHttpResponse response = createPostConnection("https://api.infolocale.fr/auth/signin", params, false);            
+            if ("initTokens".equals(type)) {
+                String login = channel.getProperty("jcmsplugin.socle.infolocale.login");
+                String password = channel.getProperty("jcmsplugin.socle.infolocale.password");
+                
+                params.put("login", login);
+                params.put("password", password);
+            } else if ("refresh_token".equals(type)) {
+                params.put("refresh_token", tokenManager.getRefreshToken());
+            }
+            
+            CloseableHttpResponse response = createPostConnection(url, params, false);
             
             if (Util.isEmpty(response)) {
-                LOGGER.warn("Method initTokens => pas de réponse HTTP" + pleaseCheckConf);
+                LOGGER.warn("Method " + type + " => pas de réponse HTTP" + pleaseCheckConf);
                 return;
             }
             
@@ -85,57 +101,6 @@ public class RequestManager {
                     LOGGER.debug("Tokens générés");
                     break;
                 case 400:
-                    LOGGER.warn(methodInitTokensError + status + ". Il manque des paramètres.");
-                    break;
-                case 401:
-                    LOGGER.warn(methodInitTokensError + status + ". Authentification échouée.");
-                    break;
-                default:
-                    LOGGER.warn(methodInitTokensError + status + pleaseCheckConf);
-                    break;
-            }
-            
-        } catch (IOException e) {
-            LOGGER.warn("Exception sur initTokens : " + e.getMessage());
-        }
-    }
-    
-    /**
-     * Regénère les tokens d'authentification. En cas de token invalide, procède à l'authentification
-     */
-    public static void regenerateTokens() {
-
-        boolean invalidToken = false;
-        
-        TokenManager tokenManager = TokenManager.getInstance();
-        
-        Map<String, Object> params = new HashMap<>();
-        params.put("refresh_token", tokenManager.getRefreshToken());
-        
-        try {
-            
-            CloseableHttpResponse response = createPostConnection("https://api.infolocale.fr/auth/refresh-token", params, false);
-            
-            if (Util.isEmpty(response)) {
-                LOGGER.warn("Method regenerateTokens => pas de réponse HTTP" + pleaseCheckConf);
-                return;
-            }
-            
-            int status = response.getStatusLine().getStatusCode();
-                        
-            switch (status) {
-                case 200:
-                    ObjectMapper mapper = new ObjectMapper();
-                    
-                    Authentification auth = mapper.readValue(response.getEntity().getContent(), Authentification.class);
-                    
-                    tokenManager.setAccessToken(auth.getAccess_token());
-                    tokenManager.setRefreshToken(auth.getRefresh_token());
-                    tokenManager.setUsername(auth.getUsername());
-                    
-                    LOGGER.debug("Tokens regénérés");
-                    break;
-                case 400:
                     LOGGER.warn(methodRegenerateTokensError + status + ". Token non valide.");
                     invalidToken = true;
                     break;
@@ -148,70 +113,14 @@ public class RequestManager {
             }
             
         } catch (IOException e) {
-            LOGGER.warn("Exception sur regenerateTokens : " + e.getMessage());
+            LOGGER.warn("Exception sur " + type + " : " + e.getMessage());
         }
         
-        if (invalidToken) {
+        // Si la requête consistait à refresh les tokens, en cas d'échec, demander une initialisation des tokens
+        if (invalidToken && "refresh_token".equals(type)) {
             LOGGER.debug("Token invalide. Tentative de regénération de token via authentification...");
             initTokens();
         }
-    }
-    
-    /**
-     * Extrait des données à partir d'un flux de l'API Infolocale
-     */
-    public static JSONObject extractFluxData(String fluxId) {
-        
-        JSONObject fluxData = new JSONObject();
-        
-        boolean expiredToken = false;
-        
-        Map<String, Object> params = new HashMap<>();
-        
-        try {
-            fluxData.put(success, false); // sera remplacé par "true" dans les cas de requête réussie
-            
-            CloseableHttpResponse response = createPostConnection("https://api.infolocale.fr/flux/" + fluxId + "/data", params, true);
-            
-            if (Util.isEmpty(response)) {
-                LOGGER.warn("Method extractFluxData => pas de réponse HTTP" + pleaseCheckConf);
-                return fluxData;
-            }
-            
-            int status = response.getStatusLine().getStatusCode();
-                        
-            switch (status) {
-                case 200:
-                    
-                    fluxData = new JSONObject(SocleUtils.convertStreamToString(response.getEntity().getContent()));
-                    fluxData.put(success, true);
-                    
-                    break;
-                case 401:
-                    LOGGER.warn(methodExtractFluxDataError + status + ". Token expiré.");
-                    expiredToken = true;
-                    fluxData.put(failureReason, "invalid_token");
-                    break;
-                case 404:
-                    LOGGER.warn(methodExtractFluxDataError + status + ". Flux " + fluxId + " non trouvé.");
-                    fluxData.put(failureReason, "wrong_flux");
-                    break;
-                default:
-                    LOGGER.warn(methodExtractFluxDataError + status + pleaseCheckConf);
-                    fluxData.put(failureReason, "unknown_error");
-                    break;
-            }
-            
-        } catch (Exception e) {
-            LOGGER.warn("Exception sur regenerateTokens : " + e.getMessage());
-        }
-        
-        if (expiredToken) {
-            LOGGER.debug("Token invalide. Tentative de regénération de token...");
-            regenerateTokens();
-        }
-        
-        return fluxData;
     }
 
     /**
@@ -267,6 +176,13 @@ public class RequestManager {
         }
         
         return fluxData;
+    }
+    
+    /**
+     * Extrait des données à partir d'un flux de l'API Infolocale sans paramètre
+     */
+    public static JSONObject filterFluxData(String fluxId) {
+        return filterFluxData(fluxId, null);
     }
     
     /**
