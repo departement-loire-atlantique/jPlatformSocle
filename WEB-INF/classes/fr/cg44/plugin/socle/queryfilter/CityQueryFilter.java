9d9a37be15e2c5cac95ca17f1ca7edeafc003920
mformont <44978720+mformont@users.noreply.github.com>
2020-04-08 09:50:16 +0200

    Fix : Correction ordre bo pour cat liée et indentation (#240)
    * Fix : Correction ordre bo pour cat liée et indentation

--- WEB-INF/classes/fr/cg44/plugin/socle/queryfilter/CityQueryFilter.java
+++ WEB-INF/classes/fr/cg44/plugin/socle/queryfilter/CityQueryFilter.java
@@ -33,20 +33,20 @@ public class CityQueryFilter extends LuceneQueryFilter {
 	 * @param cityData
 	 */
 	public void addCitySearch(QueryHandler qh, HttpServletRequest request, String... cityCode) {		
-		if(Util.notEmpty(cityCode)) {
-			// Passe la query en syntaxe avancée pour accepter les requêtes lucenes
-			qh.setMode(QueryHandler.TEXT_MODE_ADVANCED);	  
-			// Requête pour la recherche sur les communes (OR entre les communes)	
-			StringBuffer citySearchText = new StringBuffer();
-			for(String itCityCode : cityCode) {
-				if(Util.notEmpty(citySearchText.toString())) {
-					citySearchText.append(" OR ");
-				}
-				citySearchText.append(PublicationFacetedSearchCityEnginePolicyFilter.INDEX_FIELD_CITY + ":\"" + itCityCode + "\"");								
-			}
-			// Requêtes pour incrémenter la recherche des communes avec les précédants query des autres facettes						
-	    	addFacetQuery(qh, request, citySearchText.toString());
-		}
+	  if(Util.notEmpty(cityCode)) {
+	    // Passe la query en syntaxe avancée pour accepter les requêtes lucenes
+	    qh.setMode(QueryHandler.TEXT_MODE_ADVANCED);	  
+	    // Requête pour la recherche sur les communes (OR entre les communes)	
+	    StringBuffer citySearchText = new StringBuffer();
+	    for(String itCityCode : cityCode) {
+	      if(Util.notEmpty(citySearchText.toString())) {
+	        citySearchText.append(" OR ");
+	      }
+	      citySearchText.append(PublicationFacetedSearchCityEnginePolicyFilter.INDEX_FIELD_CITY + ":\"" + itCityCode + "\"");								
+	    }
+	    // Requêtes pour incrémenter la recherche des communes avec les précédants query des autres facettes						
+	    addFacetQuery(qh, request, citySearchText.toString());
+	  }
 	}
 	
 	
