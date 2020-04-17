ade9372722cd92172cbc1ca3d792ed0b451d71ef
Mickael <mickael.formont@trsb.net>
2020-04-17 20:45:44 +0200

    Ajout de la facette delegation

---
+++ WEB-INF/classes/fr/cg44/plugin/socle/queryfilter/DelegationQueryFilter.java
@@ -0,0 +1,41 @@
+package fr.cg44.plugin.socle.queryfilter;
+
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+
+import com.jalios.jcms.Channel;
+import com.jalios.jcms.QueryResultSet;
+import com.jalios.jcms.handler.QueryHandler;
+import com.jalios.util.Util;
+
+import generated.City;
+import generated.Delegation;
+
+
+/**
+ * Filtre pour la facette delegation.
+ */
+public class DelegationQueryFilter extends LuceneQueryFilter {
+
+	
+  @Override
+  public QueryHandler doFilterQuery(QueryHandler qh, Map context, HttpServletRequest request) {	
+    // Récupère la délégation en paramètre de recherche
+    String delegationCodeSearch = request.getParameter("delegation");
+    // Récupère les communes de la delegation pour la recherche à facettes
+    // Rechercher sur la delegation revient à recherche sur toutes les communes de cette délégation
+    Delegation delegation = (Delegation) Channel.getChannel().getPublication(delegationCodeSearch);
+    if(Util.notEmpty(delegation)) {     
+      addCitySearch(qh, request, delegation.getLinkIndexedDataSet(City.class, "delegation").stream().map(x -> String.valueOf(x.getCityCode())).toArray(String[]::new));
+    }
+    return qh;
+  }
+	
+	
+	@Override
+	public QueryResultSet doFilterResult(QueryHandler qh, QueryResultSet set, Map context, HttpServletRequest request) {
+		return set;
+	}
+
+}
