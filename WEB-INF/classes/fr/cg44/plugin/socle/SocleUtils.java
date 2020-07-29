f33e9c048dd269f636a8db648c37315f88ecb9e7
sguellec <48445572+sguellec@users.noreply.github.com>
2020-07-29 16:13:19 +0200

    Evol : Fiche commune (RS-849) (#503)
    * Evol fiche commune
    
    Suppression de champs inutiles + changement de type pour l'adresse.
    Ajout d'un tag pour la génération d'une bannière  "push" pour l'accès au site "vuduciel".
    Déclaration du gabarit par défaut
    Ajout de la bannière push

--- WEB-INF/classes/fr/cg44/plugin/socle/SocleUtils.java
+++ WEB-INF/classes/fr/cg44/plugin/socle/SocleUtils.java
@@ -463,8 +463,29 @@ public final class SocleUtils {
 				fichelieu.getNdeVoie(), fichelieu.getLibelleDeVoie(), fichelieu.getLieudit(),
 				"", fichelieu.getCodePostal(), Util.notEmpty(fichelieu.getCommune()) ? fichelieu.getCommune().getTitle() : "", "");
 	}	
-		
 	
+
+	
+	/**
+	 * <p>Concatène et formate toutes les infos d'une adresse en un String à partir d'une Commune sous la forme suivante :</p>
+	 * <p>[adresse mairie]<br\></p>
+	 * <p>CS [cs]<br\></p>
+	 * <p>[codePostal] [commune]</p>
+	 * 
+	 * @param commune
+	 * @return un String contenant l'adresse physique de la commune
+	 */
+	public static String formatAdresseCommune(City city) {
+	
+		String adresseMairie = city.getCouncilBuildingAddress().replaceAll("[\\r\\n]+", "");
+		
+	    if(adresseMairie.replaceAll("[\\r\\n]+", "").lastIndexOf("</p></div>") != -1){
+	        adresseMairie = adresseMairie.substring(0, adresseMairie.replaceAll("[\\r\\n]+", "").lastIndexOf("</p></div>"));
+	    }
+	    
+	    return adresseMairie + "<br/>" + SocleUtils.formatAddress(null, null, null, null, null, null, city.getPostalBox(), city.getZipCode(), city.getTitle(), null) + "</p></div>";
+	}	
+    
 	
 	/**
 	 * Créé une url oppenstreetmap à partir des coordonnées et du zoom souhaité
