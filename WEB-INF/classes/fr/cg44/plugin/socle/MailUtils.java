504f3a72f050071e062c7203c59becc7c6395ee8
sguellec <48445572+sguellec@users.noreply.github.com>
2020-08-11 16:47:27 +0200

    Evol : formulaire d'envoi de FAQ (#521)
    Modif du gabarit full pour intégrer le vrai formulaire d'envoi.
    Ajout d'une classe de contrôle de la saisie.
    Ajout de la méthode d'envoi du mail.

--- WEB-INF/classes/fr/cg44/plugin/socle/MailUtils.java
+++ WEB-INF/classes/fr/cg44/plugin/socle/MailUtils.java
@@ -10,8 +10,10 @@ import org.apache.log4j.Logger;
 
 import com.jalios.jcms.Channel;
 import com.jalios.jcms.JcmsUtil;
+import com.jalios.jcms.Publication;
 import com.jalios.jcms.context.JcmsContext;
 import com.jalios.jcms.mail.MailMessage;
+import com.jalios.util.ServletUtil;
 import com.jalios.util.Util;
 
 import generated.CandidatureForm;
@@ -20,181 +22,217 @@ import generated.ContactForm;
 
 public final class MailUtils {
 	private static Channel channel = Channel.getChannel();
-	
+
 	private static final Logger LOGGER = Logger.getLogger(MailUtils.class);
-	
+
 	private MailUtils() {
 		throw new IllegalStateException("Utility class");
 	}
-	
- /**
-  * Envoi d'un email de contact.
-  */
- public static void envoiMailContact(ContactForm form, String emailTo) {
- 		String jsp = "/plugins/SoclePlugin/jsp/mail/formulaireContactTemplate.jsp";
- 	
-   // Objet
-   String objet = JcmsUtil.glp(channel.getDefaultAdmin().getLanguage(), "jcmsplugin.socle.email.contact.objet", form.getSujet(channel.getDefaultAdmin()).first().getName());
 
-   // Contenu
-   HashMap<Object, Object> parametersMap = new HashMap<Object, Object>();
-   parametersMap.put("nom", form.getNom());
-   parametersMap.put("prenom", form.getPrenom());
-   parametersMap.put("email", form.getMail());
-   parametersMap.put("telephone", Util.notEmpty(form.getTelephone()) ? form.getTelephone() : "");
-   parametersMap.put("codepostal", form.getCodePostal());
-   parametersMap.put("commune", SocleUtils.getCitynameFromZipcode(form.getCodePostal()));
-   parametersMap.put("sujet", form.getSujet(channel.getDefaultAdmin()).first());
-   parametersMap.put("message", form.getMessage());
-   
-   try {
-     sendMail(objet, null, form.getMail(), emailTo, null, jsp, parametersMap);
-     msgEnvoiMailContact();
-   } catch (Exception e) {
-     msgEchecEnvoiMailContact();
-     LOGGER.error("Erreur lors de l'envoi du mail" + e.getMessage());
-   }
+	/**
+	 * Envoi d'un email de contact.
+	 */
+	public static void envoiMailContact(ContactForm form, String emailTo) {
+		String jsp = "/plugins/SoclePlugin/jsp/mail/formulaireContactTemplate.jsp";
 
- }
+		// Objet
+		String objet = JcmsUtil.glp(channel.getDefaultAdmin().getLanguage(), "jcmsplugin.socle.email.contact.objet", form.getSujet(channel.getDefaultAdmin()).first().getName());
 
- /**
-  * Envoi du mail de candidature spontanée.
-  */
- public static void envoiMailCandidatureSpontanee(CandidatureSpontaneeForm form, ArrayList<File> fichiers) {
+		// Contenu
+		HashMap<Object, Object> parametersMap = new HashMap<Object, Object>();
+		parametersMap.put("nom", form.getNom());
+		parametersMap.put("prenom", form.getPrenom());
+		parametersMap.put("email", form.getMail());
+		parametersMap.put("telephone", Util.notEmpty(form.getTelephone()) ? form.getTelephone() : "");
+		parametersMap.put("codepostal", form.getCodePostal());
+		parametersMap.put("commune", SocleUtils.getCitynameFromZipcode(form.getCodePostal()));
+		parametersMap.put("sujet", form.getSujet(channel.getDefaultAdmin()).first());
+		parametersMap.put("message", form.getMessage());
+
+		try {
+			sendMail(objet, null, form.getMail(), emailTo, null, jsp, parametersMap);
+			msgEnvoiMailContact();
+		} catch (Exception e) {
+			msgEchecEnvoiMailContact();
+			LOGGER.error("Erreur lors de l'envoi du mail" + e.getMessage());
+		}
+
+	}
+
+	/**
+	 * Envoi du mail de candidature spontanée.
+	 */
+	public static void envoiMailCandidatureSpontanee(CandidatureSpontaneeForm form, ArrayList<File> fichiers) {
 		String jsp = "/plugins/SoclePlugin/jsp/mail/formulaireCandidatureSpontaneeTemplate.jsp";
- 	
-  // Objet
-  String objet = JcmsUtil.glp(channel.getDefaultAdmin().getLanguage(), "jcmsplugin.socle.email.candidature-spontanee.objet");
 
-  // Contenu
-  HashMap<Object, Object> parametersMap = new HashMap<Object, Object>();
-  parametersMap.put("nom", form.getNom());
-  parametersMap.put("prenom", form.getPrenom());
-  parametersMap.put("email", form.getMail());
-  parametersMap.put("telephone", Util.notEmpty(form.getTelephone()) ? form.getTelephone() : "");
-  parametersMap.put("codepostal", form.getCodePostal());
-  parametersMap.put("commune", SocleUtils.getCitynameFromZipcode(form.getCodePostal()));
-  parametersMap.put("nature", form.getNatureRecherche(channel.getDefaultAdmin()).first());
+		// Objet
+		String objet = JcmsUtil.glp(channel.getDefaultAdmin().getLanguage(), "jcmsplugin.socle.email.candidature-spontanee.objet");
+
+		// Contenu
+		HashMap<Object, Object> parametersMap = new HashMap<Object, Object>();
+		parametersMap.put("nom", form.getNom());
+		parametersMap.put("prenom", form.getPrenom());
+		parametersMap.put("email", form.getMail());
+		parametersMap.put("telephone", Util.notEmpty(form.getTelephone()) ? form.getTelephone() : "");
+		parametersMap.put("codepostal", form.getCodePostal());
+		parametersMap.put("commune", SocleUtils.getCitynameFromZipcode(form.getCodePostal()));
+		parametersMap.put("nature", form.getNatureRecherche(channel.getDefaultAdmin()).first());
 
 		String emailTo = channel.getProperty("jcmsplugin.socle.form.candidature.mailTo");
 
 		if (Util.notEmpty(emailTo)) {
 			try {
 				sendMail(objet, null, form.getMail(), emailTo, fichiers, jsp, parametersMap);
-    msgEnvoiMailContact();
-    } catch (Exception e) {
-    	msgEchecEnvoiMailContact();
-    	LOGGER.error("Erreur lors de l'envoi du mail de candidature spontanée" + e.getMessage());
-    }
+				msgEnvoiMailContact();
+			} catch (Exception e) {
+				msgEchecEnvoiMailContact();
+				LOGGER.error("Erreur lors de l'envoi du mail de candidature spontanée" + e.getMessage());
+			}
 
 		} else {
 			msgEchecEnvoiMailContact();
 			LOGGER.error("Le mail de candidature spontanée n'a pas pu être envoyé car l'email par destination n'est pas défini.");
 		}
- }
+	}
 
- /**
-  * Envoi d'un email de candidature (réponse à une offre).
-  */
- public static void envoiMailCandidature(CandidatureForm form, ArrayList<File> fichiers) {
+	/**
+	 * Envoi d'un email de candidature (réponse à une offre).
+	 */
+	public static void envoiMailCandidature(CandidatureForm form, ArrayList<File> fichiers) {
 		String jsp = "/plugins/SoclePlugin/jsp/mail/formulaireCandidatureTemplate.jsp";
- 	
-  // Objet
-  String objet = JcmsUtil.glp(channel.getDefaultAdmin().getLanguage(), "jcmsplugin.socle.email.candidature.objet", form.getReference());
 
-  // Contenu
-  HashMap<Object, Object> parametersMap = new HashMap<Object, Object>();
-  parametersMap.put("nom", form.getNom());
-  parametersMap.put("prenom", form.getPrenom());
-  parametersMap.put("email", form.getMail());
-  parametersMap.put("telephone", Util.notEmpty(form.getTelephone()) ? form.getTelephone() : "");
-  parametersMap.put("codepostal", form.getCodePostal());
-  parametersMap.put("commune", SocleUtils.getCitynameFromZipcode(form.getCodePostal()));
+		// Objet
+		String objet = JcmsUtil.glp(channel.getDefaultAdmin().getLanguage(), "jcmsplugin.socle.email.candidature.objet", form.getReference());
+
+		// Contenu
+		HashMap<Object, Object> parametersMap = new HashMap<Object, Object>();
+		parametersMap.put("nom", form.getNom());
+		parametersMap.put("prenom", form.getPrenom());
+		parametersMap.put("email", form.getMail());
+		parametersMap.put("telephone", Util.notEmpty(form.getTelephone()) ? form.getTelephone() : "");
+		parametersMap.put("codepostal", form.getCodePostal());
+		parametersMap.put("commune", SocleUtils.getCitynameFromZipcode(form.getCodePostal()));
 
 		String emailTo = channel.getProperty("jcmsplugin.socle.form.candidature.mailTo");
 
 		if (Util.notEmpty(emailTo)) {
 			try {
 				sendMail(objet, null, form.getMail(), emailTo, fichiers, jsp, parametersMap);
-    msgEnvoiMailContact();
-    } catch (Exception e) {
-    	msgEchecEnvoiMailContact();
-    	LOGGER.error("Erreur lors de l'envoi du mail de candidature spontanée" + e.getMessage());
-    }
+				msgEnvoiMailContact();
+			} catch (Exception e) {
+				msgEchecEnvoiMailContact();
+				LOGGER.error("Erreur lors de l'envoi du mail de candidature spontanée" + e.getMessage());
+			}
 
 		} else {
 			msgEchecEnvoiMailContact();
 			LOGGER.error("Le mail de candidature spontanée n'a pas pu être envoyé car l'email par destination n'est pas défini.");
 		}
- } 
+	} 
 
- /**
-  * Envoi de mail avec le pied de mail du CG44.
-  * 
-  * @param subject Objet du mail
-  * @param content Contenu du mail
-  * @param emailFrom Email de l'expéditeur
-  * @param emailTo Email du destinataire
-  * @param listePieceJointe Liste des pièces jointes
-  * @param jsp La JSP du template de mail à envoyer
-  * @param parametersMap La map contenant les données à placer dans le template JSP
-  */
- public static void sendMail(String subject, String content, String emailFrom, String emailTo, ArrayList<File> listePieceJointe, String jsp, HashMap<Object, Object> parametersMap)
-     throws javax.mail.MessagingException {
+	/**
+	 * Envoi de l'email du formaulaire FAQ.
+	 */
+	public static void envoiMailFAQ(HttpServletRequest request, String emailTo) {
+		String jsp = "/plugins/SoclePlugin/jsp/mail/formulaireFaqTemplate.jsp";
+		
+		// Objet
+		String objet = JcmsUtil.glpd("jcmsplugin.socle.email.faq.objet");
 
-   MailMessage mail = new MailMessage("Département de Loire Atlantique");
-   mail.setFrom(emailFrom);
-   mail.setTo(emailTo);
-   mail.setSubject(subject);
-   
-   if(Util.notEmpty(content)) {
-   	mail.setContentHtml(content);
-   }else if(Util.notEmpty(jsp)) {
-   	mail.setContentHtmlFromJsp(jsp, channel.getDefaultAdmin(), "fr", parametersMap, null);
-   }
+		// Publication concernée par la FAQ
+		Publication pub = channel.getPublication(request.getParameter("id"));
+		// Contenu
+		HashMap<Object, Object> parametersMap = new HashMap<Object, Object>();
+		parametersMap.put("publication", pub.getTitle());
+		parametersMap.put("lien", ServletUtil.getBaseUrl(request)+pub.getDisplayUrl(channel.getLocale()));
+		parametersMap.put("question", request.getParameter("question"));
+		parametersMap.put("codepostal", Util.notEmpty(request.getParameter("commune")) ? request.getParameter("commune") : "");
+		parametersMap.put("commune", Util.notEmpty(request.getParameter("commune[text]")) ? request.getParameter("commune[text]") : "");
+		parametersMap.put("email", request.getParameter("mail"));
 
-   if (Util.notEmpty(listePieceJointe)) {
-     for (File f : listePieceJointe) {
-       mail.addFile(f);
-     }
-   }
-   // Envoi du mail
-   mail.send();
- }
- 
- /**
-  * Envoi du message de confirmation de l'envoi du mail.
-  */
- public static void msgEnvoiMailContact() {
-   HttpServletRequest request = channel.getCurrentServletRequest();
-   JcmsContext.setInfoMsgSession(JcmsUtil.glpd("jcmsplugin.socle.email.message.succes"), request);
-   LOGGER.debug(JcmsUtil.glpd("jcmsplugin.socle.email.message.succes"));
- }
+		if (Util.notEmpty(emailTo)) {
+			try {
+				sendMail(objet, null, request.getParameter("mail"), emailTo, null, jsp, parametersMap);
+				msgEnvoiMailContact();
+			} catch (Exception e) {
+				msgEchecEnvoiMailContact();
+				LOGGER.error("Erreur lors de l'envoi du mail de FAQ" + e.getMessage());
+			}
 
- /**
-  * Envoi du message d'erreur d'envoi du mail.
-  */
- public static void msgEchecEnvoiMailContact() {
-   HttpServletRequest request = channel.getCurrentServletRequest();
-   JcmsContext.setErrorMsgSession(JcmsUtil.glpd("jcmsplugin.socle.email.message.echec"), request);
-   LOGGER.warn(JcmsUtil.glpd("jcmsplugin.socle.email.message.echec"));
- }  
- 
- /**
-  * Ajout des clauses à la fin du message.
-  * 
-  * @param contenu
-  *          le message du mail.
-  * @return le message avec les clauses.
-  */
- public static String addClause(String contenu) {
-   StringBuilder sb = new StringBuilder(contenu);
-   sb.append("<br />");
-   sb.append("Ce message a &eacute;t&eacute; envoy&eacute; &agrave; partir d'une adresse ne pouvant recevoir d'e-mails. Merci de ne pas y r&eacute;pondre.<br/>");
-   sb.append("<br />");
-   sb.append("---------------------------<br/>");
-   sb.append("<br />");
-   sb.append("En application de la loi n&ordm; 78-17 du 6 janvier 1978 relative &agrave; l'informatique, aux fichiers et aux libert&eacute;s, vous disposez des droits d'opposition, d'acc&egrave;s et de rectification des donn&eacute;es vous concernant. Ainsi, vous pouvez demander une mise &agrave; jour ou une suppression des informations vous concernant si elles s'av&egrave;rent inexactes, incompl&egrave;tes, &eacute;quivoques, p&eacute;rim&eacute;es ou si leur collecte ou leur utilisation, communication ou conservation est interdite.");
-   return sb.toString();
- } 
+		} else {
+			msgEchecEnvoiMailContact();
+			LOGGER.error("Le mail de FAQ n'a pas pu être envoyé car l'email par destination n'est pas défini.");
+		}
+
+	}
+
+	/**
+	 * Envoi de mail avec le pied de mail du CG44.
+	 * 
+	 * @param subject Objet du mail
+	 * @param content Contenu du mail
+	 * @param emailFrom Email de l'expéditeur
+	 * @param emailTo Email du destinataire
+	 * @param listePieceJointe Liste des pièces jointes
+	 * @param jsp La JSP du template de mail à envoyer
+	 * @param parametersMap La map contenant les données à placer dans le template JSP
+	 */
+	public static void sendMail(String subject, String content, String emailFrom, String emailTo, ArrayList<File> listePieceJointe, String jsp, HashMap<Object, Object> parametersMap)
+			throws javax.mail.MessagingException {
+
+		MailMessage mail = new MailMessage("Département de Loire Atlantique");
+		mail.setFrom(emailFrom);
+		mail.setTo(emailTo);
+		mail.setSubject(subject);
+
+		if(Util.notEmpty(content)) {
+			mail.setContentHtml(content);
+		}else if(Util.notEmpty(jsp)) {
+			mail.setContentHtmlFromJsp(jsp, channel.getDefaultAdmin(), "fr", parametersMap, null);
+		}
+
+		if (Util.notEmpty(listePieceJointe)) {
+			for (File f : listePieceJointe) {
+				mail.addFile(f);
+			}
+		}
+		// Envoi du mail
+		mail.send();
+	}
+
+	/**
+	 * Envoi du message de confirmation de l'envoi du mail.
+	 */
+	public static void msgEnvoiMailContact() {
+		HttpServletRequest request = channel.getCurrentServletRequest();
+		JcmsContext.setInfoMsgSession(JcmsUtil.glpd("jcmsplugin.socle.email.message.succes"), request);
+		LOGGER.debug(JcmsUtil.glpd("jcmsplugin.socle.email.message.succes"));
+	}
+
+	/**
+	 * Envoi du message d'erreur d'envoi du mail.
+	 */
+	public static void msgEchecEnvoiMailContact() {
+		HttpServletRequest request = channel.getCurrentServletRequest();
+		JcmsContext.setErrorMsgSession(JcmsUtil.glpd("jcmsplugin.socle.email.message.echec"), request);
+		LOGGER.warn(JcmsUtil.glpd("jcmsplugin.socle.email.message.echec"));
+	}  
+
+	/**
+	 * Ajout des clauses à la fin du message.
+	 * 
+	 * @param contenu
+	 *          le message du mail.
+	 * @return le message avec les clauses.
+	 */
+	public static String addClause(String contenu) {
+		StringBuilder sb = new StringBuilder(contenu);
+		sb.append("<br />");
+		sb.append("Ce message a &eacute;t&eacute; envoy&eacute; &agrave; partir d'une adresse ne pouvant recevoir d'e-mails. Merci de ne pas y r&eacute;pondre.<br/>");
+		sb.append("<br />");
+		sb.append("---------------------------<br/>");
+		sb.append("<br />");
+		sb.append("En application de la loi n&ordm; 78-17 du 6 janvier 1978 relative &agrave; l'informatique, aux fichiers et aux libert&eacute;s, vous disposez des droits d'opposition, d'acc&egrave;s et de rectification des donn&eacute;es vous concernant. Ainsi, vous pouvez demander une mise &agrave; jour ou une suppression des informations vous concernant si elles s'av&egrave;rent inexactes, incompl&egrave;tes, &eacute;quivoques, p&eacute;rim&eacute;es ou si leur collecte ou leur utilisation, communication ou conservation est interdite.");
+		return sb.toString();
+	} 
 }
---
+++ plugins/SoclePlugin/types/FaqAccueil/doFaqAccueilFullDisplayContent.jsp
@@ -0,0 +1,43 @@
+<%@ page contentType="text/html; charset=UTF-8" %>
+
+
+	
+		<div class="grid-12-small-1">
+			<div class="col-6-small-1">
+				<h3 class="h4-like" aria-level="2" role="heading"><%= Util.notEmpty(obj.getSoustitre(userLang)) ? obj.getSoustitre(userLang) : glp("jcmsplugin.socle.faq.consulter-question-frequente") %></h3>
+				<ul class="ds44-collapser ds44-mb-std ">
+					<jalios:foreach name="itQuestRep" type="FaqEntry" collection='<%= obj.getLinkIndexedDataSet(FaqEntry.class) %>' counter='nbrQuestRep'>
+						<li class='ds44-collapser_element <%= nbrQuestRep > obj.getNombreDeQuestionsAffichees() ? "hidden" : "" %>'>
+							<button type="button" class="ds44-collapser_button">
+								<%= itQuestRep.getTitle(userLang) %>
+								<i class="icon icon-down" aria-hidden="true"></i>
+							</button>
+							<div class="ds44-collapser_content">
+								<ul class="ds44-list ds44-collapser_content--level2">
+									<li>
+										<jalios:wysiwyg><%= itQuestRep.getAnswer(userLang) %></jalios:wysiwyg>
+									</li>
+								</ul>
+							</div>
+						</li>
+					</jalios:foreach>
+				</ul>
+				<jalios:if predicate='<%= obj.getLinkIndexedDataSet(FaqEntry.class).size() > obj.getNombreDeQuestionsAffichees() %>'>
+					<button class="ds44-btnStd ds44-btnStd--large ds44-js-more-button" type="button" title='<%= glp("jcmsplugin.socle.faq.afficher-plus-questions") %>'>
+						<span class="ds44-btnInnerText"><%= glp("jcmsplugin.socle.faq.plus-questions") %></span>
+						<i class="icon icon-long-arrow-right" aria-hidden="true"></i>
+					</button>
+				</jalios:if>
+			</div>
+			<div class="col-2-small-1 txtcenter">
+				<div class="ds44-separator ds44-flex-valign-center ds44-flex-align-center ds44-flex-container">
+					<p class="ds44-txtBulle ds44-theme"><%= glp("jcmsplugin.socle.ou") %></p>
+				</div>
+			</div>
+			<div class="col-4-small-1">
+                <%-- Inclusion du formulaire --%>
+                <%@ include file='doFaqForm.jspf' %>
+			</div>
+		</div>
+
+
