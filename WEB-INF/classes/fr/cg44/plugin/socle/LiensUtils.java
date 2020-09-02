9320be7df490990cdcfcdaf8628d372443854f82
Unknown <stephane.guellec@loire-atlantique.fr>
2020-09-02 17:09:56 +0200

    Fix : lien de partage twitter

--- WEB-INF/classes/fr/cg44/plugin/socle/LiensUtils.java
+++ WEB-INF/classes/fr/cg44/plugin/socle/LiensUtils.java
@@ -60,7 +60,7 @@ public final class LiensUtils {
 		StringBuffer link = new StringBuffer("https://twitter.com/intent/tweet?url=");
 		link.append(HttpUtil.encodeForURL(url));
 		link.append("&amp;tw_p=tweetbutton");
-		link.append("&amp;via=@");
+		link.append("&amp;via=");
 		link.append(channel.getProperty("jcmsplugin.socle.socialnetwork.twitter.account"));
 
 		return link.toString();
