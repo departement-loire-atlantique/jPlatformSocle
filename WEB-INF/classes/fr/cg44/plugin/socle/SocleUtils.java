829a08508c66661740b5ca5c454d78864b34ef6b
formont <mickael.formont@trsb.net>
2020-09-04 17:10:50 +0200

    Evol : Fiches liens dans les annuaires

--- WEB-INF/classes/fr/cg44/plugin/socle/SocleUtils.java
+++ WEB-INF/classes/fr/cg44/plugin/socle/SocleUtils.java
@@ -50,6 +50,7 @@ import generated.Delegation;
 import generated.ElectedMember;
 import generated.FicheEmploiStage;
 import generated.FicheLieu;
+import generated.Lien;
 import generated.PortletAgendaInfolocale;
 import generated.PortletFacetteAdresse;
 import generated.PortletFacetteCategoriesLiees;
@@ -697,13 +698,28 @@ public final class SocleUtils {
 	 */
 	public static JsonObject publicationToJsonObject(Publication pub, String pubListGabarit, String pubMarkerGabarit, String pubFullGabarit) {
 		JsonObject jsonObject = new JsonObject();
-		jsonObject.addProperty("id", pub instanceof Canton ? String.valueOf(((Canton) (pub)).getCantonCode()) : pub.getId());
+		String id = pub instanceof Canton ? String.valueOf(((Canton) (pub)).getCantonCode()) : pub.getId();
+		
 		jsonObject.addProperty("value", pub.getTitle());
 		if(Util.notEmpty(pubFullGabarit)) {
 		  jsonObject.addProperty("content_html", pubFullGabarit);
-    }
+		}
 		JsonObject jsonMetaObject = new JsonObject();
-		jsonMetaObject.addProperty("url", channel.getUrl() + pub.getDisplayUrl(null));
+		// Cas particulier pour les type de contenu Lien
+		String url = channel.getUrl() + pub.getDisplayUrl(null);
+		if(pub instanceof Lien) {
+			Lien lien = (Lien) pub;
+			if(Util.notEmpty(lien.getLienInterne())) {
+				url = channel.getUrl() + lien.getLienInterne().getDisplayUrl(null);
+				id =  lien.getLienInterne().getId();
+			} else {
+				url = lien.getLienExterne();
+				jsonObject.addProperty("redirectUrl", true);
+				jsonObject.addProperty("target", "_blank");
+			}			
+		}
+		jsonObject.addProperty("id", id);
+		jsonMetaObject.addProperty("url", url);
 		jsonMetaObject.addProperty("type", pub.getClass().getSimpleName());
 		jsonMetaObject.addProperty("lat", pub.getExtraData("extra."+ pub.getClass().getSimpleName() +".plugin.tools.geolocation.latitude"));
 		jsonMetaObject.addProperty("long", pub.getExtraData("extra."+ pub.getClass().getSimpleName() + ".plugin.tools.geolocation.longitude"));
