c5195a73f87e963936dbfb078f0303a1487c6f6c
Mickael <mickael.formont@trsb.net>
2020-09-18 19:00:25 +0200

    1295 : Le comparateur marche maintenant sur toutes les publications

--- WEB-INF/classes/fr/cg44/plugin/socle/comparator/ServicesDepartementComparator.java
+++ WEB-INF/classes/fr/cg44/plugin/socle/comparator/ServicesDepartementComparator.java
@@ -2,18 +2,23 @@ package fr.cg44.plugin.socle.comparator;
 
 import java.util.Set;
 
+import org.apache.log4j.Logger;
+
+import com.jalios.jcms.Category;
 import com.jalios.jcms.Channel;
 import com.jalios.jcms.JcmsUtil;
-import com.jalios.jcms.Member;
+import com.jalios.jcms.Publication;
 import com.jalios.jcms.comparator.BasicComparator;
 import com.jalios.util.Util;
 
 import fr.cg44.plugin.socle.SocleUtils;
 import generated.City;
-import generated.FicheLieu;
 
-public class ServicesDepartementComparator extends BasicComparator<FicheLieu>{
 
+public class ServicesDepartementComparator extends BasicComparator<Publication>{
+
+  private static final Logger LOGGER = Logger.getLogger(ServicesDepartementComparator.class);
+  
   Channel channel = Channel.getChannel();
 
   private City commune;
@@ -25,49 +30,87 @@ public class ServicesDepartementComparator extends BasicComparator<FicheLieu>{
     }
   }
 
-  public int compare(FicheLieu fiche_1, FicheLieu fiche_2) {
+  public int compare(Publication pub_1, Publication pub_2) {
 
 
-    if (fiche_1 == null) {
-      return (fiche_2 == null) ? 0 : -1;
+    if (pub_1 == null) {
+      return (pub_2 == null) ? 0 : -1;
     }
-    if (fiche_2 == null) {
+    if (pub_2 == null) {
       return 1;
     }
 
     // Comparaison sur service du département
-    int serviceComparator = comparatorService(fiche_1, fiche_2);
+    int serviceComparator = comparatorService(pub_1, pub_2);
     if(serviceComparator != 0) {
       return serviceComparator;
     }
-
+    
+    // Commune de la publication 1
+    City commune_pub_1 = null;
+    try {
+      commune_pub_1 = (City) pub_1.getFieldValue("commune");
+    } catch (NoSuchFieldException e) {      
+      try {
+        City[] communes_pub_1 = (City[]) pub_1.getFieldValue("communes");
+        commune_pub_1 = Util.getFirst(communes_pub_1);
+      } catch (NoSuchFieldException e1) {
+        LOGGER.trace("Le contenu : " + pub_1 + " n'a pas de référence à commune à indexer", e);
+      }           
+    }
+    
+    // Commune de la publication 2
+    City commune_pub_2 = null;
+    try {
+      commune_pub_2 = (City) pub_2.getFieldValue("commune");
+    } catch (NoSuchFieldException e) {
+      try {
+        City[] communes_pub_2 = (City[]) pub_2.getFieldValue("communes");
+        commune_pub_2 = Util.getFirst(communes_pub_2);
+      } catch (NoSuchFieldException e1) {
+        LOGGER.trace("Le contenu : " + pub_2 + " n'a pas de référence à commune à indexer", e);
+      }     
+    }
+    
     // Comparaison sur la commune de recherche
-    int cityComparator = comparatorCity(fiche_1, fiche_2);
+    int cityComparator = comparatorCity(commune_pub_1, commune_pub_2);
     if(cityComparator != 0) {
       return cityComparator;
     }
     
     // Comparaison sur les commune de la délégation
-    int communeDelegationComparator = comparatorCommuneDelegation(fiche_1, fiche_2);
+    int communeDelegationComparator = comparatorCommuneDelegation(commune_pub_1, commune_pub_2);
     if(communeDelegationComparator != 0) {
       return communeDelegationComparator;
     }
+    
+    // Si une publication n'a pas de commune n'est pas prioritaire sur une publication avec commune
+    if(commune_pub_1 != null && commune_pub_2 == null) {
+      return -1;
+    }else if(commune_pub_1 == null && commune_pub_2 != null) {
+      return 1;
+    }
+    
+    // Ordre alphabétique des communes
+    if(commune_pub_1 != null && commune_pub_2 != null && commune_pub_1.compareTo(commune_pub_2) != 0) {
+      return commune_pub_1.getTitle().compareTo(commune_pub_2.getTitle());
+    }
 
-    return super.compare(fiche_1, fiche_2);
+    return super.compare(pub_1, pub_2);
   }
 
 
   /**
    * Compare avec la catégorie services du département
-   * @param fiche_1
-   * @param fiche_2
+   * @param pub_1
+   * @param pub_2
    * @return
    */
-  private int comparatorService(FicheLieu fiche_1, FicheLieu fiche_2) {
-    Member loggedMember = channel.getCurrentLoggedMember();
-    if(Util.notEmpty(fiche_1.getServiceDuDepartement(loggedMember)) && Util.isEmpty(fiche_2.getServiceDuDepartement(loggedMember))) {
+  private int comparatorService(Publication pub_1, Publication pub_2) {
+    Category serviceDuDepartementCat = channel.getCategory("$jcmsplugin.socle.fichelieu.serviceDepartement.root");    
+    if(pub_1.containsCategory(serviceDuDepartementCat) && !pub_2.containsCategory(serviceDuDepartementCat)) {
       return -1;
-    }else if(Util.notEmpty(fiche_2.getServiceDuDepartement(loggedMember)) && Util.isEmpty(fiche_1.getServiceDuDepartement(loggedMember))) {
+    }else if(!pub_1.containsCategory(serviceDuDepartementCat) && pub_2.containsCategory(serviceDuDepartementCat)) {
       return 1;
     }
     return 0;
@@ -76,19 +119,17 @@ public class ServicesDepartementComparator extends BasicComparator<FicheLieu>{
 
   /**
    * Compare à la commune de recherche
-   * @param fiche_1
-   * @param fiche_2
+   * @param pub_1
+   * @param pub_2
    * @return
    */
-  private int comparatorCity(FicheLieu fiche_1, FicheLieu fiche_2) {
-    City commune_fiche_1 = fiche_1.getCommune();
-    City commune_fiche_2 = fiche_2.getCommune();
+  private int comparatorCity(City commune_pub_1, City commune_pub_2) {    
     if(Util.isEmpty(commune)) {
       return 0;
     }
-    if(JcmsUtil.isSameId(commune, commune_fiche_1) && !JcmsUtil.isSameId(commune, commune_fiche_2)) {
+    if(JcmsUtil.isSameId(commune, commune_pub_1) && !JcmsUtil.isSameId(commune, commune_pub_2)) {
       return -1;
-    }else if(!JcmsUtil.isSameId(commune, commune_fiche_1) && JcmsUtil.isSameId(commune, commune_fiche_2)) {
+    }else if(!JcmsUtil.isSameId(commune, commune_pub_1) && JcmsUtil.isSameId(commune, commune_pub_2)) {
       return 1;
     }
     return 0;
@@ -97,22 +138,21 @@ public class ServicesDepartementComparator extends BasicComparator<FicheLieu>{
   
   /**
    * Compare au commune de la même délégation
-   * @param fiche_1
-   * @param fiche_2
+   * @param pub_1
+   * @param pub_2
    * @return
    */
-  private int comparatorCommuneDelegation(FicheLieu fiche_1, FicheLieu fiche_2) {
-    City commune_fiche_1 = fiche_1.getCommune();
-    City commune_fiche_2 = fiche_2.getCommune();
+  private int comparatorCommuneDelegation(City commune_pub_1, City commune_pub_2) {
+
     if(Util.isEmpty(commune) || Util.isEmpty(commune.getDelegation())) {
       return 0;
     } 
     
     Set<City> cityList = channel.getLinkIndexedDataSet(commune.getDelegation(), City.class, "delegation");
     
-    if(cityList.contains(commune_fiche_1) && !cityList.contains(commune_fiche_2)) {
+    if((commune_pub_1 != null && cityList.contains(commune_pub_1)) && (commune_pub_2 == null || !cityList.contains(commune_pub_2))) {
       return -1;
-    }else if(!cityList.contains(commune_fiche_1) && cityList.contains(commune_fiche_2)) {
+    }else if((commune_pub_1 == null || !cityList.contains(commune_pub_1)) && (commune_pub_2 != null && cityList.contains(commune_pub_2))) {
       return 1;
     }
     return 0;
