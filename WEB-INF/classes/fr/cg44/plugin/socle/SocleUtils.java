45ce2047254523a9e2942c3e52a4aaf32d0ab4d5
formont <mickael.formont@trsb.net>
2020-09-22 15:12:19 +0200

    Utilisation de l'extradata synonyme

--- WEB-INF/classes/fr/cg44/plugin/socle/SocleUtils.java
+++ WEB-INF/classes/fr/cg44/plugin/socle/SocleUtils.java
@@ -1539,7 +1539,7 @@ public final class SocleUtils {
       while(!JcmsUtil.isSameId(typeCat.getParent(), typeRootCat)) {
         typeCat = typeCat.getParent();
       }
-      libelle = Util.notEmpty(typeCat.getExtraData("extra.Category.plugin.tools.result.title")) ? typeCat.getExtraData("extra.Category.plugin.tools.result.title") : typeCat.getName() ;    
+      libelle = Util.notEmpty(typeCat.getExtraData("extra.Category.plugin.tools.synonyme.facet.title")) ? typeCat.getExtraData("extra.Category.plugin.tools.synonyme.facet.title") : typeCat.getName() ;    
     }else if(pub instanceof AccueilAnnuaireAgenda) {
       // Pour l'agenda/annuaire
       AccueilAnnuaireAgenda accueil = (AccueilAnnuaireAgenda) pub;
