f2486d8cdc0500d85b4306ad6c7485bf0fdb5e82
formont <mickael.formont@trsb.net>
2020-09-22 15:07:50 +0200

    Nom type de l'emploi en extraData

--- WEB-INF/classes/fr/cg44/plugin/socle/SocleUtils.java
+++ WEB-INF/classes/fr/cg44/plugin/socle/SocleUtils.java
@@ -1539,7 +1539,7 @@ public final class SocleUtils {
       while(!JcmsUtil.isSameId(typeCat.getParent(), typeRootCat)) {
         typeCat = typeCat.getParent();
       }
-      libelle = Util.notEmpty(typeCat.getSynonyms()) ? Util.getFirst(typeCat.getSynonyms()) : typeCat.getName() ;    
+      libelle = Util.notEmpty(typeCat.getExtraData("extra.Category.plugin.tools.result.title")) ? typeCat.getExtraData("extra.Category.plugin.tools.result.title") : typeCat.getName() ;    
     }else if(pub instanceof AccueilAnnuaireAgenda) {
       // Pour l'agenda/annuaire
       AccueilAnnuaireAgenda accueil = (AccueilAnnuaireAgenda) pub;
