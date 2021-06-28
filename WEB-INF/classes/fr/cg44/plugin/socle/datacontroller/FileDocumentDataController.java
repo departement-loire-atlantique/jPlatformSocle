b2b9e9c3a3eb0880f04eaa8c5b07e736bc7f0063
LChoquet <lucas.choquet@trsb.net>
2021-06-28 09:51:20 +0200

    Modif pour propriété éditable

--- WEB-INF/classes/fr/cg44/plugin/socle/datacontroller/FileDocumentDataController.java
+++ WEB-INF/classes/fr/cg44/plugin/socle/datacontroller/FileDocumentDataController.java
@@ -9,22 +9,25 @@ import com.jalios.jcms.Data;
 import com.jalios.jcms.FileDocument;
 import com.jalios.jcms.Member;
 import com.jalios.jcms.plugin.PluginComponent;
+import com.jalios.util.Util;
 
 public class FileDocumentDataController extends BasicDataController implements PluginComponent {
 
 	private static Channel channel = Channel.getChannel();
 
-	private static final Category classementCategory = channel.getCategory("$jcmsplugin.socle.category.classementfiledoc.root");
+	private static final Category classementCategory = channel.getCategory(channel.getProperty("jcmsplugin.socle.category.classementfiledoc"));
 
 	@Override
 	/* 
 	 * Ajoute une catégorie de classement sur les filedocuments (RS-1199)
 	 * */
 	public void beforeWrite(Data data, int op, Member mbr, Map context) {
-
-	    FileDocument itFileDoc = (FileDocument) data;
 	    
-	    itFileDoc.addCategory(classementCategory);
+	    if (Util.notEmpty(classementCategory)) {
+    	    FileDocument itFileDoc = (FileDocument) data;
+    	    
+    	    itFileDoc.addCategory(classementCategory);
+	    }
 	
 	}
 
